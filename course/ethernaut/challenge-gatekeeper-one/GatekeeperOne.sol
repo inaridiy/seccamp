5ca1cd614e9f6281e676eff6186c4f8b2db3e299
inaridiy <inainainariinainari@gmail.com>
2023-06-27 20:54:57 +0900

    power

--- course/ethernaut/challenge-gatekeeper-one/GatekeeperOne.sol
+++ course/ethernaut/challenge-gatekeeper-one/GatekeeperOne.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "forge-std/console.sol";
+
 contract GatekeeperOne {
     address public entrant;
 
@@ -15,13 +17,24 @@ contract GatekeeperOne {
     }
 
     modifier gateThree(bytes8 _gateKey) {
-        require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
-        require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
-        require(uint32(uint64(_gateKey)) == uint16(uint160(tx.origin)), "GatekeeperOne: invalid gateThree part three");
+        require(
+            uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)),
+            "GatekeeperOne: invalid gateThree part one"
+        );
+        require(
+            uint32(uint64(_gateKey)) != uint64(_gateKey),
+            "GatekeeperOne: invalid gateThree part two"
+        );
+        require(
+            uint32(uint64(_gateKey)) == uint16(uint160(tx.origin)),
+            "GatekeeperOne: invalid gateThree part three"
+        );
         _;
     }
 
-    function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
+    function enter(
+        bytes8 _gateKey
+    ) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
         entrant = tx.origin;
         return true;
     }
